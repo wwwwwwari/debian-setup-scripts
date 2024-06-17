836669841f2601492318fbee41e57bd1d9347bd3
Wari <89649458+wwwwwwari@users.noreply.github.com>
2024-06-17 22:00:08 +0700

    Delete files when uninstalled

--- flatpak_snap_copier.sh
+++ flatpak_snap_copier.sh
@@ -10,21 +10,36 @@ source_snap_dir="/var/lib/snapd/desktop/applications"
 # Destination directory where files will be copied
 destination_dir="$HOME/.local/share/applications"
 
-# Find files in source directory and iterate over them
+# Copy files from flatpak desktop dir if not already exists (only check file name so the local ones can be customized)
 find "$source_flatpak_dir" -iname "*.desktop" | while read -r file; do
 	# do not copy if file exists
 	base_name="$(basename ${file})" 
-	if [ ! -f "$destination_dir/$base_name" ]; then
+	new_file="$destination_dir/$base_name"
+	if [ ! -f "$new_file" ]; then
 		cp -n "$file" "$destination_dir"
+		chmod +x "$new_file"
 		zenity --notification --text="Copied flatpak file: $file"
 	fi
 done
+# Copy files from snap desktop dir if not already exists (only check file name so the local ones can be customized)
 find "$source_snap_dir" -iname "*.desktop" | while read -r file; do
 	# do not copy if file exists
 	base_name="$(basename ${file})" 
-	if [ ! -f "$destination_dir/$base_name" ]; then
+	new_file="$destination_dir/$base_name"
+	if [ ! -f "$new_file" ]; then
 		cp -n "$file" "$destination_dir"
+		chmod +x "$new_file"
 		zenity --notification --text="Copied snap file: $file"
+	fi
+done
+# remove if outdated
+find "$destination_dir" -iname "*.desktop" | while read -r file; do
+	base_name="$(basename ${file})" 
+	old_flatpak_file="$source_flatpak_dir/$base_name"
+	old_snap_file="$source_snap_dir/$base_name"
+	if [ ! -f "$old_flatpak_file" ] && [ ! -f "$old_snap_file" ]; then
+		rm -f $file
+		zenity --notification --text="Deleted outdated desktop file: $file"
 	fi
 done
 jwm -reload
